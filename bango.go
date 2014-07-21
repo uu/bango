65d73d9b2addc9e3b2877f04f24ef574d81ed7b3
Michael Pirogov <vbnet.ru@gmail.com>
2014-07-21 17:40:38 +0400

    Added unban action.

--- bango.go
+++ bango.go
@@ -5,96 +5,109 @@
 package main
 
 import (
-	//	"strconv"
-	//	"fmt"
-	"log"
-	"gopkg.in/redis.v1"
-	"runtime"
-	"os"
-	"os/exec"
-	//	"syscall"
-	//	"reflect"
-	"github.com/Unknwon/goconfig"
-	"net"
+    //	"strconv"
+    //	"fmt"
+    "log"
+    "gopkg.in/redis.v1"
+    "runtime"
+    "os"
+    "strings"
+    "os/exec"
+    //	"syscall"
+    //	"reflect"
+    "github.com/Unknwon/goconfig"
+    "net"
 )
 
 var client *redis.Client
 
 func init() {
-	LoadBangoConfig("/etc/bango.ini")
+    LoadBangoConfig("/etc/bango.ini")
 }
 
 func main() {
-	action, ip := ParseFlags()
-	runtime.GOMAXPROCS(4)
-	CreateConnection()
+    action, ip := ParseFlags()
+    runtime.GOMAXPROCS(4)
+    CreateConnection()
 
-	switch action {
-	case "publish":
-		BangoPublish("fail2ban", ip)
-	default:
-		BangoSubscribe()
-	}
+    switch action {
+    case "publish":
+        BangoPublish("fail2ban", ip)
+    default:
+        BangoSubscribe()
+    }
 }
 
 func CreateConnection() {
-	var serverstring = ""
-	serverstring = config.redis.server+":"+config.redis.port
+    var serverstring = ""
+    serverstring = config.redis.server+":"+config.redis.port
 
-	client = redis.NewTCPClient(&redis.Options{
-										Addr:     serverstring,
-										Password: config.redis.pass,
-										DB:       int64(config.redis.db),})
+    client = redis.NewTCPClient(&redis.Options{
+                                        Addr:     serverstring,
+                                        Password: config.redis.pass,
+                                        DB:       int64(config.redis.db),})
 
-	pong, err := client.Ping().Result()
+    pong, err := client.Ping().Result()
 
-	if err == nil {
-		log.Println("Connected succesfully and got", pong, "from redis://" + serverstring)
-	}
+    if err == nil {
+        log.Println("Connected succesfully and got", pong, "from redis://" + serverstring)
+    }
 }
 
 func BangoSubscribe() {
-	pubsub := client.PubSub()
-	defer pubsub.Close()
+    pubsub := client.PubSub()
+    defer pubsub.Close()
+    var unbanprefix = "unban"
 
-	err := pubsub.Subscribe("fail2ban")
-	_ = err
+    err := pubsub.Subscribe("fail2ban")
+    _ = err
 
-	for {
-		msg, err := pubsub.Receive()
-		_ = err
+    for {
+        msg, err := pubsub.Receive()
+        _ = err
 
-		switch subscr := msg.(type) {
-		case *redis.Subscription:
-			log.Println("Subscribed sucessfully to 'fail2ban' channel")
-		case *redis.Message:
-			log.Println("Received:", subscr.Channel, subscr.Payload)
-			BanIP(subscr.Payload)
-		default:
-			panic("ERROR: Something went wrong")
-		}
-	}
+        switch subscr := msg.(type) {
+        case *redis.Subscription:
+            log.Println("Subscribed sucessfully to 'fail2ban' channel")
+        case *redis.Message:
+            log.Println("Received:", subscr.Channel, subscr.Payload)
+            if strings.HasPrefix(subscr.Payload, unbanprefix) {
+                UnBanIP(strings.TrimPrefix(subscr.Payload,unbanprefix))
+            } else {
+                BanIP(subscr.Payload)
+            }
+        default:
+            panic("ERROR: Something went wrong")
+        }
+    }
 
 }
 
 func BanIP(ip string) {
-	if CheckIP(ip) {
-		ExecCommand("fail2ban-client", "set", config.fail2ban.jail, "banip", ip)
-	}
+    if CheckIP(ip) {
+        ExecCommand("fail2ban-client", "set", config.fail2ban.jail, "banip", ip)
+    }
+}
+
+func UnBanIP(ip string) {
+    if CheckIP(ip) {
+        ExecCommand("fail2ban-client", "set", config.fail2ban.jail, "unbanip", ip)
+    }
 }
 
 
 
+
 func BangoPublish(channel, ip string) {
-	log.Println("Starting publish in 'fail2ban' channel")
-	if CheckIP(ip) {
-		pubsub := client.PubSub()
-		defer pubsub.Close()
-		pub := client.Publish(channel, ip)
-		if pub.Err() == nil {
-			log.Printf("Successfully published '%s'", ip)
-		}
-	}
+    log.Println("Starting publish in 'fail2ban' channel")
+    if CheckIP(ip) {
+        pubsub := client.PubSub()
+        defer pubsub.Close()
+        pub := client.Publish(channel, ip)
+        if pub.Err() == nil {
+            log.Printf("Successfully published '%s'", ip)
+        }
+    }
 
 }
 
@@ -103,67 +116,67 @@ func BangoPublish(channel, ip string) {
 //
 
 const (
-	version = "0.1"
+    version = "0.1"
 )
 
 // Packaged all Server settings
 type Config struct {
-	redis    Redis
-	global   Global
-	fail2ban Fail2ban
+    redis    Redis
+    global   Global
+    fail2ban Fail2ban
 }
 
 type Redis struct {
-	server string
-	port   string
-	db     int
-	pass   string
+    server string
+    port   string
+    db     int
+    pass   string
 }
 
 type Global struct {
-	debug   bool
-	logFile string
+    debug   bool
+    logFile string
 }
 
 type Fail2ban struct {
-	channel string
-	jail    string
-	useF2C  bool
+    channel string
+    jail    string
+    useF2C  bool
 }
 
 // Define a global config varible
 var config Config
 
 func LoadBangoConfig(fileName string) {
-	var err error
-	_, err = os.Stat(fileName)
+    var err error
+    _, err = os.Stat(fileName)
 
-	if err != nil {
-		if os.IsNotExist(err) {
-			panic("Configuration file does not exists: " + err.Error())
-		} else {
-			panic("Something wrong with configuration file: " + err.Error())
-		}
-	}
+    if err != nil {
+        if os.IsNotExist(err) {
+            panic("Configuration file does not exists: " + err.Error())
+        } else {
+            panic("Something wrong with configuration file: " + err.Error())
+        }
+    }
 
-	var cfg *goconfig.ConfigFile
-	cfg, err = goconfig.LoadConfigFile(fileName)
-	if err != nil {
-		panic("Fail to load configuration file: " + err.Error())
-	}
-	// Parse the global section
-	config.global.debug = cfg.MustBool("global", "debug", false)
+    var cfg *goconfig.ConfigFile
+    cfg, err = goconfig.LoadConfigFile(fileName)
+    if err != nil {
+        panic("Fail to load configuration file: " + err.Error())
+    }
+    // Parse the global section
+    config.global.debug = cfg.MustBool("global", "debug", false)
 
-	// Parse the redis section
-	config.redis.server = cfg.MustValue("redis", "server", "localhost")
-	config.redis.port = cfg.MustValue("redis", "port", "6379")
-	config.redis.db = cfg.MustInt("redis", "db", 0)
-	config.redis.pass = cfg.MustValue("redis", "pass", "")
+    // Parse the redis section
+    config.redis.server = cfg.MustValue("redis", "server", "localhost")
+    config.redis.port = cfg.MustValue("redis", "port", "6379")
+    config.redis.db = cfg.MustInt("redis", "db", 0)
+    config.redis.pass = cfg.MustValue("redis", "pass", "")
 
-	// Parse the fail2ban section
-	config.fail2ban.channel = cfg.MustValue("fail2ban", "channel", "fail2ban")
-	config.fail2ban.jail = cfg.MustValue("fail2ban", "jail", "fail2ban-recidive")
-	config.fail2ban.useF2C = cfg.MustBool("fail2ban", "usef2bclient", true)
+    // Parse the fail2ban section
+    config.fail2ban.channel = cfg.MustValue("fail2ban", "channel", "fail2ban")
+    config.fail2ban.jail = cfg.MustValue("fail2ban", "jail", "fail2ban-recidive")
+    config.fail2ban.useF2C = cfg.MustBool("fail2ban", "usef2bclient", true)
 }
 
 //
@@ -171,49 +184,49 @@ func LoadBangoConfig(fileName string) {
 //
 
 func CheckIP(checkip string) bool {
-	trial := net.ParseIP(checkip)
-	if trial.To4() == nil {
-		log.Printf("ERROR: '%v' is not an IPv4 address. Doing nothing.\n", checkip)
-		return false
-	} else {
-		//		s := trial.To4()
-		//		log.Println(reflect.TypeOf(s))
-		return true
-	}
+    trial := net.ParseIP(checkip)
+    if trial.To4() == nil {
+        log.Printf("ERROR: '%v' is not an IPv4 address. Doing nothing.\n", checkip)
+        return false
+    } else {
+        //		s := trial.To4()
+        //		log.Println(reflect.TypeOf(s))
+        return true
+    }
 }
 
 func ExecCommand(command string, args ...string) {
-	binary, lookErr := exec.LookPath(command)
-	if lookErr != nil {
-		panic(lookErr)
-	}
+    binary, lookErr := exec.LookPath(command)
+    if lookErr != nil {
+        panic(lookErr)
+    }
 
-	log.Println("Launching:", command, args)
+    log.Println("Launching:", command, args)
 
-	// actually call
-	cmd := exec.Command(binary, args...)
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
+    // actually call
+    cmd := exec.Command(binary, args...)
+    err := cmd.Start()
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	log.Println("Waiting for command to finish...")
-	err = cmd.Wait()
-	if err != nil {
-		log.Printf("Command finished with error: %v", err)
-	}
-	log.Printf("done")
+    log.Println("Waiting for command to finish...")
+    err = cmd.Wait()
+    if err != nil {
+        log.Printf("Command finished with error: %v", err)
+    }
+    log.Printf("done")
 }
 
 func ParseFlags() (string, string) {
-	var action, ip string = "", ""
-	if len(os.Args) > 2 {
-		// action: subscribe, publish
-		// default: subscribe
-		action = os.Args[1]
-		// ip address to ban
-		// default: ""
-		ip = os.Args[2]
-	}
-	return action, ip
+    var action, ip string = "", ""
+    if len(os.Args) > 2 {
+        // action: subscribe, publish
+        // default: subscribe
+        action = os.Args[1]
+        // ip address to ban
+        // default: ""
+        ip = os.Args[2]
+    }
+    return action, ip
 }
